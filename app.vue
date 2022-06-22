32bfc0df9ab34965ef4cb79a5de2dbb841643e10
Victor Furtado <victor.furtado@coden3.com.br>
2022-06-22 09:48:46 -0300

    mobile

--- app.vue
+++ app.vue
@@ -6,10 +6,10 @@ const frase = frases[Math.floor(Math.random() * frases.length)];
 </script>
 
 <template>
-  <div class="w-screen h-screen bg-gradient-to-tr from-gray-700 to-black flex items-center justify-between">
-    <img src="/dalai-lama.png" alt="Dalai Lama" class="h-full pt-5 pl-16">
+  <div class="w-screen h-screen bg-gradient-to-tr from-gray-700 to-black flex flex-col-reverse md:flex-row items-center justify-between">
+    <img src="/dalai-lama.png" alt="Dalai Lama" class="md:h-full mt-auto md:mt-0 pt-5 pl-1 md:pl-16">
     <div class="w-full">
-      <div class="mx-auto rounded-lg text-white h-52 w-96 space-y-1">
+      <div class="mx-auto pt-48 px-4 rounded-lg text-white h-52 w-96 space-y-1">
         <p class="text-right text-3xl text">"{{ frase }}"</p>
         <h1 class="text-right text-sm">Dalai Lama, 2022</h1>
       </div>
